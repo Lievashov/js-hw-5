5ce58018e75ce3eb474341623f868c8c772a49ef
Ihor Lievashov <levashov.i.v.85@gmail.com>
2020-12-18 01:18:00 +0200

    Create task4.js
    решение задачи 4

---
+++ js/task4.js
@@ -0,0 +1,34 @@
+ // Write code under this line
+  class StringBuilder {
+      constructor(line) {
+          this._value = line;
+      }
+      get value() {
+          return this._value;
+      }
+      append(str) {
+          return (this._value += str);
+      }
+      prepend(str) {
+          return (this._value = str + this._value);
+      }
+      pad(str) {
+          this.append(str);
+          this.prepend(str);
+      }
+  }
+
+ console.log(typeof StringBuilder);
+// 'function'
+
+
+ const builder = new StringBuilder('.');
+
+ builder.append('^');
+ console.log(builder.value); // '.^'
+
+ builder.prepend('^');
+ console.log(builder.value); // '^.^'
+
+ builder.pad('=');
+ console.log(builder.value); // '=^.^='
